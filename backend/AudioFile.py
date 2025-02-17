f565e91ddc3b1036afcc4fcd4a601f960fbac7ac
n33vb4k <bakshi.neev@gmail.com>
2025-02-17 23:20:39 +0000

    added test folder subdir, added getDuration

--- backend/AudioFile.py
+++ backend/AudioFile.py
@@ -1,18 +1,22 @@
 import os
 from pydub import AudioSegment
 
-class AudioFile:
+class Audio:
     def __init__(self, filePath):
         self.filePath = filePath
         self.fileType = filePath.split('.')[-1].lower()
         self.fileName = filePath.split('/')[-1]
         self.fileSize = os.path.getsize(filePath)
+        if not self.validateFormat():
+            raise ValueError('Invalid file format. Please provide a .mp3 or .wav file.')
 
     def loadFile(self):
         return AudioSegment.from_file(self.filePath)
     
+    def getDuration(self):
+        return self.loadFile().duration_seconds
+    
     def saveFile(self, outputFilePath):
-        # This method saves the original file.
         self.loadFile().export(outputFilePath, format=self.fileType)
         return outputFilePath
     
---
+++ backend/tests/test.py
@@ -0,0 +1,26 @@
+import sys
+import os
+
+# Add the parent directory to sys.path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from AudioFile import Audio
+from AudioProcessor import AudioProcessingService
+
+def test():
+    path = 'backend/tests/test1.mp3'
+    # Create an Audio instance
+    audioFile = Audio(path)
+    print("Input file duration: ", audioFile.getDuration())
+    
+    # Create the processing service instance
+    processor = AudioProcessingService(audioFile)
+    
+    # Process the audio (cutting it as specified)
+    processor.processAudio([(2020, 3020)])
+    print("Processed file duration: ", processor.audio.duration_seconds)
+
+    # Save the processed audio using the processor's method
+    processor.saveFile('backend/tests/test_processed1.mp3')
+
+test()
