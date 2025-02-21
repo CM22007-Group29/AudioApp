52dc9ffe969e9c3e5634673e77d066270abc76e4
Grace Steele <gssteeleuk@gmail.com>
2025-02-21 15:52:50 +0000

    basic dropzone for file upload

--- frontend/src/components/audio_remove/FileUpload.tsx
+++ frontend/src/components/audio_remove/FileUpload.tsx
@@ -1,7 +1,31 @@
-const FileUpload: React.FC<{}> = () => {
+import { useCallback } from "react"
+import { useDropzone } from "react-dropzone";
+
+function FileUpload() {
+    const {acceptedFiles, getRootProps, getInputProps} = useDropzone({
+        accept: {"audio/*": []},
+        multiple: false,
+    });
+  
+    const files = acceptedFiles.map(file => (
+        <li key={file.path}>
+        {file.path} - {file.size} bytes
+        </li>
+    ));
+
     return (
-        <div className="text-white">Dropzone</div>
-    )
+        <section>
+            <div {...getRootProps()} className="flex-1 flex flex-col items-center text-center p-[20px] border-2 rounded-2xl border-dashed border-gray-300 bg-[#f5f5f5] hover:bg-gray-200 transition text-[#212121] outline-none">
+                <input {...getInputProps()} />
+                <p className="font-semibold mb-[12]">Drop files here, or <button className="text-[#1269cf] cursor-pointer">click</button> to select files</p>
+            </div>
+            <aside className="p-[20px]">
+                <h4 className="text-[#f5f5f5]">Files</h4>
+                <ul>{files}</ul>
+            </aside>
+        </section>
+        
+    );
 }
 
 export default FileUpload;
--- frontend/src/components/audio_remove/GetStarted.tsx
+++ frontend/src/components/audio_remove/GetStarted.tsx
@@ -6,8 +6,11 @@ function GetStarted() {
         <div className="relative h-screen bg-radial from-[#383838] to-[#262626]">
           <div className="relative pt-16 ml-auto">
             <div className="lg:w-2/3 text-center mx-auto">
-              <h1 className="text-gray-900 text-balance dark:text-white font-bold text-3xl md:text-4xl xl:text-5xl">What are we editing today?</h1>
-              <FileUpload />
+              <h1 className="text-gray-900 text-balance dark:text-[#f5f5f5] font-bold text-3xl md:text-4xl xl:text-5xl">What are we editing today?</h1>
+              <div className="mt-14">
+                <FileUpload />
+              </div>
+              
             </div>
           </div>
         </div>
