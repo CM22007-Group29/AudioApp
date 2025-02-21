e6f33be4b6fab12fbc0fb8b83023cbd1824e12f7
Grace Steele <63586910+gracesteele@users.noreply.github.com>
2025-02-21 11:00:26 +0000

    final commit for homepage and nav bar
    * initial commit for basic react website
    
    * Created basic nav bar structure
    
    * working nav bar
    
    * updated nav bar with rounded edges
    
    * updated the homepage
    
    * Removed .gitignore from frontend
    
    * final commit for homepage and nav bar before splitting branches
    
    ---------
    
    Co-authored-by: Tom Cave <thomaslcave@gmail.com>

---
+++ frontend/src/App.tsx
@@ -0,0 +1,22 @@
+import Nav from "./components/nav/Nav" 
+import Home from "./components/Home"
+import { Route, Routes } from 'react-router'
+import Features from "./components/Features"
+import GetStarted from "./components/audio_remove/GetStarted"
+
+
+
+function App() {
+  return (
+    <>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="features" element={<Features />} />
+        <Route path="get-started" element={<GetStarted />} />
+      </Routes>
+    </>
+  )
+}
+
+export default App
