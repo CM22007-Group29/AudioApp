f553afb1b9d577f4dd9769322e3db9c098e2d32d
Luke Davis <lukeda117@gmail.com>
2025-02-24 17:03:49 +0000

    WIP Silence Removal

--- backend/AudioProcessor.py
+++ backend/AudioProcessor.py
@@ -1,5 +1,7 @@
 from pydub import AudioSegment, effects
 
+from AudioFile import Audio
+
 class AudioProcessingService:
     def __init__(self, audioFile):
         # Load the audio and store its type for later export.
@@ -34,6 +36,52 @@ class AudioProcessingService:
         Normalise the audio volume using pydub built in function
         """
         return effects.normalize(self.audio)
+    
+    def removeSilences(self, threshold=0.0):
+        """
+        Remove background noise in the audio, determined by a threshold given by the user
+        threshold: the threshold set by the user as a float between 0 and 1 where lower means more background noise removed
+        """
+        audio_length_s = self.audio.duration_seconds
+        audio_length_ms = audio_length_s * 1000
+
+        # Pydub uses a relative scaling for its volume
+        # So get the maxium possible volume of the audio
+        max_vol = self.audio.max_dBFS
+
+        # Dbfs gives decibels relative to full scale
+        # 0 is the loudest possible volume in the sample
+        # It then decreases logarithmically from there (-10 Dbfs is 1/10 of full volume)
+
+        # Maximum level for background noise removal will be half of max possible volume
+        max_threshold = max_vol / 2
+
+        # Then scale this by users threshold setting using log base 10
+
+
+        # Run through the audio clip and get a list of timestamps for silences
+        # In format [(start,end), ...]
+        timestamps = []
+
+        start = 0
+        end = 0
+        background = False
+        while end < audio_length_ms:
+            
+            # print(self.audio[end].dBFS)
+            if self.audio[end].dBFS <= threshold:
+                if not background:
+                    background = True
+                    start = end
+            else:
+                if background:
+                    # Add this section to timestamp list
+                    timestamps.append((start, end))
+                    background = False
+            end += 1
+        
+        print(timestamps)
+        return timestamps
 
     def processAudio(self, timestamps=[], normalize=False):
         """
@@ -41,10 +89,14 @@ class AudioProcessingService:
         Carry out other processing steps here. (e.g., noise reduction, volume normalization)
         normalise: bool that determines whether normalization happens
         """
-        if normalize:
-            self.audio = self.normalizeAudio()
+        
         if timestamps:
             return self.cutAudio(timestamps)
+        
+        # Normalisation makes sense as last step
+        if normalize:
+            self.audio = self.normalizeAudio()
+        
         return self.audio
 
     def saveFile(self, outputFilePath):
@@ -52,3 +104,20 @@ class AudioProcessingService:
         Exports the processed audio to a file.
         """
         self.audio.export(outputFilePath, format=self.fileType)
+
+
+if __name__ == "__main__":
+    # path = 'backend/tests/audio_input.mp3'
+    path = 'backend/tests/audio_input.mp3'
+
+    audioFile = Audio(path)
+    
+    # Create the processing service instance
+    processor = AudioProcessingService(audioFile)
+    
+    # Process the audio (cutting it as specified)
+    processor.removeSilences()
+
+    # Save the processed audio using the processor's method
+    # processor.saveFile('backendtests/test_processed1.mp3')
+    processor.saveFile('backend/tests/test_background_noise.mp3')
