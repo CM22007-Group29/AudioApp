9d3798687961ef1dd998038b3499791862c02d89
Luke Davis <lukeda117@gmail.com>
2025-03-02 11:44:01 +0000

    Silence removal test written

--- backend/tests/test_audioprocessor.py
+++ backend/tests/test_audioprocessor.py
@@ -43,3 +43,30 @@ def test_normalization():
     # Save the processed audio using the processor's method
     # processor.saveFile('backendtests/test_processed1.mp3')
     processor.saveFile('tests/test_processed_normalized.mp3')
+
+def test_silence_removal():
+    """
+    Test for silence removal by looking at duration of clip.
+    """
+    # path = 'backend/tests/audio_input.mp3'
+    path = 'tests/audio_input.mp3'
+    # Create an Audio instance
+    audioFile = Audio(path)
+
+    # Create the processing service instance
+    processor = AudioProcessingService(audioFile)
+
+    # Get duration before silence removal
+    original_duration = processor.audio.duration_seconds
+    
+    # Process the audio (with a silence length of 0.5 and a low threshold)
+    processor.processAudio(silence_length=1, silence_threshold=-16)
+
+    # Get duration after silence removal
+    silenced_duration = processor.audio.duration_seconds
+
+    # Save the processed audio using the processor's method to check if difference can be heard
+    processor.saveFile('tests/test_silence_removal')
+
+    # If successful silenced audio duration less than or equal to length
+    assert original_duration <= silenced_duration
