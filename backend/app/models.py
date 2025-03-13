7e9e9d46d0384713a853537ab3a71def8bd428e6
Neev Bakshi <46976045+n33vb4k@users.noreply.github.com>
2025-03-13 15:52:10 +0000

    Adding worker onto updated backend & adding relevant API endpoints (#68)
    * worker and db tests added 
    * Updated worker to be a class, now also returns the cut timestamps ready to be sent to the frontend
    * fixed test and pipeline
    * fixed module structure
    * Added routes to get and update user preferences, added routes to upload and return audio files
    * added endpoint to process audio
    * file upload now works between backend and frontend
    * updated dockerfile to run python -m backend
    * updated docker compose
    * removed db from pr

--- backend/app/models.py
+++ backend/app/models.py
@@ -9,7 +9,7 @@ class BaseModel(db.Model):
     """
     __abstract__ = True
 
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
 
     @classmethod
     def get_all(cls):
@@ -64,3 +64,52 @@ class User(BaseModel):
 
     def json(self):
         return {'id': self.id,'username': self.username, 'email': self.email}
+
+    def create_preferences(self, data):
+        data['user_id'] = self.id
+        return UserPreferences.create(data)
+    
+    def get_preferences(self):
+        return UserPreferences.query.filter_by(user_id=self.id).first()
+    
+    def upload_audio(self, data):
+        data['user_id'] = self.id
+        return Audio.create(data)
+    
+    def get_audio(self):
+        return Audio.query.filter_by(user_id=self.id).first()
+    
+
+class UserPreferences(BaseModel):
+     __tablename__ = 'user_preferences'
+
+     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+     normalise = db.Column(db.Boolean, nullable=False, default=False)
+     extra_words = db.Column(db.String, nullable=True)
+     silence_length = db.Column(db.Integer, nullable=True)
+     silence_threshold = db.Column(db.Integer, nullable=True)
+     user = db.relationship('User', backref=db.backref('preferences', lazy=True))
+
+     def json(self):
+         return {
+             'id': self.id,
+             'user_id': self.user_id,
+             'normalise': self.normalise,
+             'extra_words': self.extra_words,
+             'silence_length': self.silence_length,
+             'silence_threshold': self.silence_threshold
+         } 
+     
+class Audio(BaseModel):
+    __tablename__ = 'audio'
+
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    file_path = db.Column(db.String, nullable=False)
+    user = db.relationship('User', backref=db.backref('audio', lazy=True))
+
+    def json(self):
+        return {
+            'id': self.id,
+            'user_id': self.user_id,
+            'file_path': self.file_path
+        }
