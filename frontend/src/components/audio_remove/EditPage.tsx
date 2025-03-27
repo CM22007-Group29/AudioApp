7ea4bb8db456a01602cdd17cb1fad3fcc2830636
n33vb4k <bakshi.neev@gmail.com>
2025-03-27 19:34:20 +0000

    added audio editor to edit page

--- frontend/src/components/audio_remove/EditPage.tsx
+++ frontend/src/components/audio_remove/EditPage.tsx
@@ -1,10 +1,11 @@
-import * as React from 'react';
+import * as React from "react";
 import SilenceLengthSlider from "./user_preferences/SilenceLengthSlider";
 import SilenceThresholdSlider from "./user_preferences/SilenceThresholdSlider";
 import NormalisationCheckbox from "./user_preferences/NormalisationCheckbox";
-import Grid from '@mui/material/Grid2';
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
+import Grid from "@mui/material/Grid2";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import AudioEditor from "./AudioEditor";
 
 function EditPage() {
   const [silenceThreshold, setSilenceThreshold] = React.useState(50);
@@ -14,63 +15,92 @@ function EditPage() {
 
   return (
     <Box
-      sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '50vh' }}
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        minHeight: "50vh",
+      }}
     >
+      <Box
+        sx={{
+            backgroundColor: "white",
+            padding: 2,
+            width: "100%",
+            maxWidth: 1200,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            mb: 4,
+            mt: 4,
+            borderRadius: "12px"  // add this line for rounded corners
+        }}
+      >
+        <AudioEditor />
+      </Box>
+      
       <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
         {/* Silence Threshold */}
         <Grid size={4}>
-          <p style={{ minWidth: '40px', textAlign: 'center', color: 'white' }}>
+          <p style={{ minWidth: "40px", textAlign: "center", color: "white" }}>
             Silence Threshold
           </p>
         </Grid>
         <Grid size={4}>
-          <SilenceThresholdSlider 
-            silence_threshold={silenceThreshold} 
+          <SilenceThresholdSlider
+            silence_threshold={silenceThreshold}
             setSilenceThreshold={setSilenceThreshold}
           />
         </Grid>
         <Grid size={4}>
-          <p style={{ minWidth: '40px', textAlign: 'center', color: 'white' }}>
+          <p style={{ minWidth: "40px", textAlign: "center", color: "white" }}>
             {silenceThreshold}
           </p>
         </Grid>
         {/* Silence Length */}
         <Grid size={4}>
-          <p style={{ minWidth: '40px', textAlign: 'center', color: 'white' }}>
+          <p style={{ minWidth: "40px", textAlign: "center", color: "white" }}>
             Silence Length
           </p>
         </Grid>
         <Grid size={4}>
-          <SilenceLengthSlider 
+          <SilenceLengthSlider
             silence_length={silenceLength}
-            setSilenceLength={setSilenceLength} 
+            setSilenceLength={setSilenceLength}
           />
         </Grid>
         <Grid size={4}>
-          <p style={{ minWidth: '40px', textAlign: 'center', color: 'white' }}>
+          <p style={{ minWidth: "40px", textAlign: "center", color: "white" }}>
             {silenceLength}
           </p>
         </Grid>
         {/* Normalisation */}
         <Grid size={4}>
-          <p style={{ minWidth: '40px', textAlign: 'center', color: 'white' }}>
+          <p style={{ minWidth: "40px", textAlign: "center", color: "white" }}>
             Normalisation
           </p>
         </Grid>
-        <Grid size={4} sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-          <NormalisationCheckbox 
+        <Grid
+          size={4}
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <NormalisationCheckbox
             check_normalisation={checkedNormalisation}
             setCheckNormalisation={setCheckedNormalisation}
           />
         </Grid>
         <Grid size={4}>
-          <p style={{ minWidth: '40px', textAlign: 'center', color: 'white' }}>
-            {checkedNormalisation ? "On" : "Off" }
+          <p style={{ minWidth: "40px", textAlign: "center", color: "white" }}>
+            {checkedNormalisation ? "On" : "Off"}
           </p>
         </Grid>
         {/* Extra Words */}
         <Grid size={4}>
-          <p style={{ minWidth: '40px', textAlign: 'center', color: 'white' }}>
+          <p style={{ minWidth: "40px", textAlign: "center", color: "white" }}>
             Extra Words
           </p>
         </Grid>
@@ -84,8 +114,8 @@ function EditPage() {
             sx={{
               width: 400,
               minWidth: 300,
-              input: { color: 'white' },
-              '& .MuiInputLabel-root': { color: 'white' }
+              input: { color: "white" },
+              "& .MuiInputLabel-root": { color: "white" },
             }}
           />
         </Grid>
