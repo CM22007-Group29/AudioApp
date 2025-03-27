c6ef31c0303eb469e4b0c4f9c15f4c1d35564e1b
Umar Kamran <147610223+Umar-Kamran@users.noreply.github.com>
2025-03-27 15:17:25 +0000

    Word timestamp retrieval function (#84)
    * Adjusted file paths for audio test
    
    * Fixed erroneous function call for audio processor test
    
    * Changed model to whisperX
    
    * Adjusted linear interpolation for word removal, added more test scripts.
    
    * Testing performance and accuracy of other STT models
    
    * Bug fixes with STT model (fixed timestamps when transcript contains numbers)
    
    * Changed audio processor to use new STT model as default
    
    * removed extra files
    
    * Fixed path
    
    * Created new route and functions to return words and timestamps
    
    * Added boolean to word_timestamps, indicating whether the word has been detected as a word to cut out. Words detected by the STT as having a ... at the end have had their functionality adjusted.
    
    * Error fix of ... functionality
    
    * timestamp endpoint created with json response
    
    * Final routes version
    
    * Fixed error in wordremover function definition
    
    * Fixed problems in audio processor (duplicated functions)

--- backend/STT.py
+++ backend/STT.py
@@ -1,7 +1,15 @@
 import whisperx
+import whisperx
 
 class SpeachToText():
     def __init__(self):
+        device = "cpu" 
+        self.device = device
+        # audio_file = "audio.mp3"
+        compute_type = "float32" # change to "int8" if low on GPU mem (may reduce accuracy)
+
+        # 1. Transcribe with original whisper (batched)
+        self.model = whisperx.load_model("distil-large-v3",device=device, compute_type=compute_type,language="en")
         device = "cpu" 
         self.device = device
         # audio_file = "audio.mp3"
@@ -16,13 +24,22 @@ class SpeachToText():
 
         model_a, metadata = whisperx.load_align_model(language_code=result["language"], device=self.device)
         result = whisperx.align(result["segments"], model_a, metadata, audio, self.device, return_char_alignments=False)
+        audio = whisperx.load_audio(audioPath)
+        result = self.model.transcribe(audio,language="en")
 
+        model_a, metadata = whisperx.load_align_model(language_code=result["language"], device=self.device)
+        result = whisperx.align(result["segments"], model_a, metadata, audio, self.device, return_char_alignments=False)
+
+        output = (result["segments"]) # after alignment
+        time_tuples = []
         output = (result["segments"]) # after alignment
         time_tuples = []
         words = []
         scores = []
+        scores = []
 
         for segment in output:
+            nextFlag = False
             if "words" in segment:
                 for word_data in segment["words"]:
                     start = word_data.get("start")
@@ -30,8 +47,16 @@ class SpeachToText():
                     score = word_data.get("score") 
                     word = word_data.get("word")
                     if start is not None and end is not None and  word is not None and score is not None:
-                        time_tuples.append((start * 1000 , end * 1000))
+                        if nextFlag:
+                            time_tuples.append((int(time_tuples[-1][1]) * 0.9 + int(start * 1000) * 0.1 , int(start * 1000) * 0.7 + int(time_tuples[-1][1]) * 0.3))
+                            words.append("*...*")
+                            scores.append(1)
+                            nextFlag = False
+                        time_tuples.append((int(start * 1000) , int(end * 1000)))
                         words.append(word)
                         scores.append(score)
+                        if "..." in word:
+                            nextFlag = True
+
 
         return time_tuples, words, scores
