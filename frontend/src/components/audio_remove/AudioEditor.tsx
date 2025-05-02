668525bb5b2aeecedf8b7afd863ad69903d16bee
Umar-Kamran <umarkamran205@gmail.com>
2025-05-02 13:27:50 +0100

    lil changes (from the analysis)

--- frontend/src/components/audio_remove/AudioEditor.tsx
+++ frontend/src/components/audio_remove/AudioEditor.tsx
@@ -1,4 +1,4 @@
-import { Container, Stack } from "@mui/material"
+import { Container, Stack, Typography } from "@mui/material"
 import { AudioContextProvider } from "../AudioProvider"
 import { Transport } from "../Transport"
 import { Waveform } from "../Waveform"
@@ -6,10 +6,15 @@ import { Waveform } from "../Waveform"
 function AudioEditor() {
     return (
       <Container sx={{ marginTop: 1 }}>
-        <Stack direction="column" gap={2}>
-          <Transport />
-          <Waveform />
-        </Stack>
+        <AudioContextProvider>
+          <Stack direction="column" gap={2}>
+            <Transport />
+            <Typography variant="body2" color="text.secondary" sx={{ mt: 1, mb: 1 }}>
+              Words can be removed by clicking on the mute symbol on the respective word on the waveform
+            </Typography>
+            <Waveform />
+          </Stack>
+        </AudioContextProvider>
       </Container>
     )
 }
